91767dece41df99f1f65f429e4cf874eddf59e5c
D-GITHUB <dezamora98@gmail.com>
2023-08-11 18:29:14 -0400

    Test con librerías añadidas

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <ModbusSlave.h>
+
+void TaskReadPlugParameter( void *pvParameters );
+
+
+void setup()
+{
+}
+
+void loop()
+{
+
+}
+
+void TaskReadPlugParameter( void *pvParameters )
+{
+  
+}
