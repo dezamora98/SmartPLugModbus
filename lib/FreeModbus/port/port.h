a9819469c37509f8c7fb5ca80cbf13b765ddffec
D-GITHUB <dezamora98@gmail.com>
2023-08-19 01:59:56 -0400

    port de freemodbus para atmega328p

--- lib/FreeModbus/port/port.h
+++ lib/FreeModbus/port/port.h
@@ -1,5 +1,7 @@
 /*
  * FreeModbus Libary: AVR Port
+ * Copyright (C) 2023 Daniel E. Zamora Sifredo <dezamora@gmail.com>
+ *   - Initial version + ATmega328P support
  * Copyright (C) 2006 Christian Walter <wolti@sil.at>
  *   - Initial version + ATmega168 support
  * Modfications Copyright (C) 2006 Tran Minh Hoang:
@@ -32,134 +34,138 @@
 #include <avr/interrupt.h>
 
 /* ----------------------- Defines ------------------------------------------*/
-#define	INLINE                      inline
-#define PR_BEGIN_EXTERN_C           extern "C" {
-#define	PR_END_EXTERN_C             }
+#define INLINE inline
+#define PR_BEGIN_EXTERN_C \
+    extern "C"            \
+    {
+#define PR_END_EXTERN_C }
 
-#define ENTER_CRITICAL_SECTION( )   cli()
-#define EXIT_CRITICAL_SECTION( )    sei()
+#define ENTER_CRITICAL_SECTION() cli()
+#define EXIT_CRITICAL_SECTION() sei()
 
-#define assert( x )
+#define assert(x)
 
-typedef char    BOOL;
+typedef char BOOL;
 
 typedef unsigned char UCHAR;
-typedef char    CHAR;
+typedef char CHAR;
 
 typedef unsigned short USHORT;
-typedef short   SHORT;
+typedef short SHORT;
 
 typedef unsigned long ULONG;
-typedef long    LONG;
+typedef long LONG;
 
 #ifndef TRUE
-#define TRUE            1
+#define TRUE 1
 #endif
 
 #ifndef FALSE
-#define FALSE           0
+#define FALSE 0
 #endif
 
 /* ----------------------- AVR platform specifics ---------------------------*/
 
-#if defined (__AVR_ATmega168__)
-#define UCSRB           UCSR0B
-#define TXEN            TXEN0
-#define RXEN            RXEN0
-#define RXCIE           RXCIE0
-#define TXCIE           TXCIE0
-#define UDRE            UDRE0
-#define UBRR            UBRR0
-#define UCSRC           UCSR0C
-#define UPM1            UPM01
-#define UPM0            UPM00
-#define UCSZ0           UCSZ00
-#define UCSZ1           UCSZ01
-#define UDR             UDR0
-#define SIG_UART_TRANS  SIG_USART_TRANS
+#if defined(__AVR_ATmega168__)
+#define UCSRB UCSR0B
+#define TXEN TXEN0
+#define RXEN RXEN0
+#define RXCIE RXCIE0
+#define TXCIE TXCIE0
+#define UDRE UDRE0
+#define UBRR UBRR0
+#define UCSRC UCSR0C
+#define UPM1 UPM01
+#define UPM0 UPM00
+#define UCSZ0 UCSZ00
+#define UCSZ1 UCSZ01
+#define UDR UDR0
+#define SIG_UART_TRANS SIG_USART_TRANS
 
-#elif defined (__AVR_ATmega169__)
-#define SIG_UART_TRANS  SIG_USART_TRANS
+#elif defined(__AVR_ATmega169__)
+#define SIG_UART_TRANS SIG_USART_TRANS
 
-#elif defined (__AVR_ATmega8__)
-#define UBRR            UBRRL
-#define TCCR1C          TCCR1A  /* dummy */
-#define TIMSK1          TIMSK
-#define TIFR1           TIFR
-#define SIG_USART_DATA  SIG_UART_DATA
-#define SIG_USART_RECV  SIG_UART_RECV
+#elif defined(__AVR_ATmega8__)
+#define UBRR UBRRL
+#define TCCR1C TCCR1A /* dummy */
+#define TIMSK1 TIMSK
+#define TIFR1 TIFR
+#define SIG_USART_DATA SIG_UART_DATA
+#define SIG_USART_RECV SIG_UART_RECV
 
-#elif defined (__AVR_ATmega16__)
-#define UBRR            UBRRL
-#define TCCR1C          TCCR1A  /* dummy */
-#define TIMSK1          TIMSK
-#define TIFR1           TIFR
+#elif defined(__AVR_ATmega16__)
+#define UBRR UBRRL
+#define TCCR1C TCCR1A /* dummy */
+#define TIMSK1 TIMSK
+#define TIFR1 TIFR
 
-#elif defined (__AVR_ATmega32__)
-#define UBRR            UBRRL
-#define TCCR1C          TCCR1A  /* dummy */
-#define TIMSK1          TIMSK
-#define TIFR1           TIFR
+#elif defined(__AVR_ATmega32__)
+#define UBRR UBRRL
+#define TCCR1C TCCR1A /* dummy */
+#define TIMSK1 TIMSK
+#define TIFR1 TIFR
 
-#elif defined (__AVR_ATmega128__)
-#define UCSRB           UCSR0B
-#define UBRR            UBRR0L
-#define UDR             UDR0
-#define TIMSK1          TIMSK
-#define TIFR1           TIFR
-#define SIG_UART_TRANS  SIG_USART0_TRANS
-#define SIG_USART_DATA  SIG_USART0_DATA
-#define SIG_USART_RECV  SIG_USART0_RECV
-#define UCSZ0           UCSZ00
-#define UCSZ1           UCSZ01
-#define UPM0            UPM00
-#define UPM1            UPM01
-#define UCSRC           UCSR0C
+#elif defined(__AVR_ATmega128__)
+#define UCSRB UCSR0B
+#define UBRR UBRR0L
+#define UDR UDR0
+#define TIMSK1 TIMSK
+#define TIFR1 TIFR
+#define SIG_UART_TRANS SIG_USART0_TRANS
+#define SIG_USART_DATA SIG_USART0_DATA
+#define SIG_USART_RECV SIG_USART0_RECV
+#define UCSZ0 UCSZ00
+#define UCSZ1 UCSZ01
+#define UPM0 UPM00
+#define UPM1 UPM01
+#define UCSRC UCSR0C
 
-#elif defined (__AVR_ATmega328P__)
-#define TXEN            TXEN0
-#define RXEN            RXEN0
-#define RXCIE           RXCIE0
-#define TXCIE           TXCIE0
-#define UCSRB           UCSR0B
-#define UBRR            UBRR0
-#define UDR             UDR0
-#define SIG_UART_TRANS  USART_TX_vect
-#define SIG_USART_DATA  USART_UDRE_vect
-#define SIG_USART_RECV  USART_RX_vect
-#define SIG_OUTPUT_COMPARE1A TIMER0_COMPA_vect 
-#define UCSZ0           UCSZ00
-#define UCSZ1           UCSZ01
-#define UPM0            UPM00
-#define UPM1            UPM01
-#define UCSRC           UCSR0C
-#define UDRE            UDRE0
-
+#elif defined(__AVR_ATmega328P__)
+#define TXEN TXEN0
+#define RXEN RXEN0
+#define RXCIE RXCIE0
+#define TXCIE TXCIE0
+#define UCSRB UCSR0B
+#define UBRR UBRR0
+#define UDR UDR0
+#define SIG_UART_TRANS USART_TX_vect
+#define SIG_USART_DATA USART_UDRE_vect
+#define SIG_USART_RECV USART_RX_vect
+#define SIG_OUTPUT_COMPARE1A TIMER0_COMPA_vect
+#define UCSZ0 UCSZ00
+#define UCSZ1 UCSZ01
+#define UPM0 UPM00
+#define UPM1 UPM01
+#define UCSRC UCSR0C
+#define UDRE UDRE0
 
 #endif
 
 /* ----------------------- RS485 specifics ----------------------------------*/
-#ifdef  RTS_ENABLE
+#ifdef RTS_ENABLE
 
-#define RTS_PIN         PB0
-#define RTS_DDR         DDRB
-#define RTS_PORT        PORTB
+#define RTS_PIN PB0
+#define RTS_DDR DDRB
+#define RTS_PORT PORTB
 
-#define RTS_INIT        \
-    do { \
-        RTS_DDR |= _BV( RTS_PIN ); \
-        RTS_PORT &= ~( _BV( RTS_PIN ) ); \
-    } while( 0 );
+#define RTS_INIT                     \
+    do                               \
+    {                                \
+        RTS_DDR |= _BV(RTS_PIN);     \
+        RTS_PORT &= ~(_BV(RTS_PIN)); \
+    } while (0);
 
-#define RTS_HIGH        \
-    do { \
-        RTS_PORT |= _BV( RTS_PIN ); \
-    } while( 0 );
+#define RTS_HIGH                  \
+    do                            \
+    {                             \
+        RTS_PORT |= _BV(RTS_PIN); \
+    } while (0);
 
-#define RTS_LOW         \
-    do { \
-        RTS_PORT &= ~( _BV( RTS_PIN ) ); \
-    } while( 0 );
+#define RTS_LOW                      \
+    do                               \
+    {                                \
+        RTS_PORT &= ~(_BV(RTS_PIN)); \
+    } while (0);
 
 #endif
 
